5df9441f866eaee6e4e23fc4fc4f32743a495190
sotiris <sotiris@jsmate.net>
2019-03-31 10:59:38 +0200

    Temporarily conditional render main menu in App.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link } from "react-router-dom";
 
 class App extends Component {
+
+  isAuthorized() {
+    return false;
+  }
   render() {
+    let mainMenu = this.isAuthorized()?
+        <div className="menu">
+          <ul>
+            <li> <Link to="/">Home</Link> </li>
+            <li> <Link to="/messages">Messages</Link> </li>
+            <li> <Link to="/about">About</Link> </li>
+          </ul>
+        </div> : '';
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and do not save to reload.
           </p>
           <a
             className="App-link"
@@ -20,9 +33,9 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        {mainMenu}
       </div>
     );
   }
 }
-
 export default App;
