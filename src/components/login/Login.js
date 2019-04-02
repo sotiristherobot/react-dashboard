75b4660261cfcd5c1ee8db829c84feedff510d00
Sotiris Ioannou <sotiris@jsmate.net>
2019-04-02 16:22:41 +0200

    Added form items onChange handler, and form onSubmit handler.

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,17 +2,35 @@ import React, { Component } from 'react';
 import './Login.css';
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+        //TODO Moved state up to a higher component.
+        this.state = {
+            username: null,
+            password: null
+        };
+        this.submitButtonHandler = this.submitButtonHandler.bind(this);
+        this.changeHandler = this.changeHandler.bind(this);
+    }
+    submitButtonHandler(e) {
+        e.stopPropagation();
+        e.preventDefault();
+    }
+    changeHandler(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
     render() {
         return (
-            <form className="login-form">
+            <form method="POST" className="login-form" onSubmit={this.submitButtonHandler}>
                 <label className="login-form-item">
-                    Username:
-                    <input type="text" name="username"/>
+                    Username: <input type="text" name="username" onChange={this.changeHandler}/>
                 </label>
                 <label className="login-form-item">
-                    Password:
-                    <input type="password" name="password"/>
+                    Password: <input type="password" name="password" onChange={this.changeHandler}/>
                 </label>
+                <input className="login-form-item" type="submit" value="Submit" />
             </form>
         );
     }
