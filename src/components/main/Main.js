cfca8c4941312b9a8ed7de22682ac9e4a7739db9
Sotiris Ioannou <sotiris@jsmate.net>
2019-04-10 15:42:08 +0200

    Added basic app bar

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,18 +1,47 @@
-import React, {Component} from "react";
-import { Redirect } from "react-router-dom";
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import Security from '../../helpers/Security';
+import withStyles from '@material-ui/core/styles/withStyles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+};
 
 class Main extends Component {
-    render() {
-        return (
-            <div>
-                {
-                    !Security.isAuthorized()?
-                        <Redirect to={'/login'} />:
-                        <p>Main</p>
-                }
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className="root">
+        {
+            !Security.isAuthorized() ? <Redirect to={'/login'} /> :
+              <AppBar position="static">
+                  <Toolbar>
+                      <Typography variant="h6" color="inherit" className={classes.grow}>
+                          Welcome
+                      </Typography>
+                      <Button color="inherit">Login</Button>
+                  </Toolbar>
+              </AppBar>
+
+        }
+      </div>
+    );
+  }
 }
-export default Main;
+export default withStyles(styles)(Main);
