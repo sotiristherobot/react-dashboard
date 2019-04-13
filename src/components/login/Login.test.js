a0f853c4989c3b61c67ff7676c37432779df43cc
sotiris <sotiris@jsmate.net>
2019-04-13 21:37:40 +0200

    Write more tests to check state status.

--- src/components/login/Login.test.js
+++ src/components/login/Login.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Login from './Login';
-import { createMount } from '@material-ui/core/test-utils';
+import { createMount, createShallow } from '@material-ui/core/test-utils';
 
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -27,4 +27,16 @@ describe('<Login/>', () => {
     expect(wrapper.find('TextField').length).toBe(2);
     expect(wrapper.find('Button').length).toBe(1);
   });
+
+  it('should mutate state on textfield change event', () => {
+    const wrapper = mount(<Login />);
+    const username = wrapper.find('input[name="username"]');
+    const password = wrapper.find('input[name="password"]');
+
+    username.simulate('change', {target:{ name: 'username', value: 'sotiris'}});
+    password.simulate('change', {target:{ name: 'password', value: 'password'}});
+    expect(wrapper.find('Login').instance().state.username).toEqual('sotiris');
+    expect(wrapper.find('Login').instance().state.password).toEqual('password');
+  });
 });
+
